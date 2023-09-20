41899eb398ca207cd3398ee065d45ce9873bcc55
Marijn Vriens <marijn.vriens@houm.com>
2023-09-20 15:53:50 -0300

    Do byte->str transformation  only to specific fields.

--- flask_and_kafka/formatters.py
+++ flask_and_kafka/formatters.py
@@ -3,9 +3,10 @@ import json
 
 
 class ConsumerFormatter(logging.Formatter):
-
     def format(self, record):
         super().format(record)
+
+
         return json.dumps({
             "time": record.asctime, 
             "threadName": record.threadName,
@@ -18,15 +19,22 @@ class ConsumerFormatter(logging.Formatter):
         })
 
 
+
 class ProducerFormatter(logging.Formatter):
 
     def format(self, record):
         super().format(record)
-        for entry_name, entry_value in record.producer_log.items():
-            if isinstance(entry_value, bytes):
-                record.producer_log[entry_name] = repr(entry_value)
+        producer_log = dict(record.producer_log)  # duplicate the dict to not modify the original.
+        producer_log['key'] = convert_bytes_to_repr(producer_log['key'])
+        producer_log['value'] = convert_bytes_to_repr(producer_log['value'])
         return json.dumps({
             "time": record.asctime,
-            "threadName": record.threadName, 
-            **record.producer_log
+            "threadName": record.threadName,
+            **producer_log
         })
+
+
+def convert_bytes_to_repr(value) -> str:
+    if isinstance(value, bytes):
+        return repr(value)
+    return value
